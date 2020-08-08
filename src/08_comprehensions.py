da832400f957ff2214fed40671e25285d7cd32a8
Mikey Williams <manicdesigner@gmail.com>
2020-08-08 13:28:56 -0400

    complete comprehensions 8

--- src/08_comprehensions.py
+++ src/08_comprehensions.py
@@ -36,6 +36,6 @@ print(y)
 x = input("Enter comma-separated numbers: ").split(',')
 
 # What do you need between the square brackets to make it work?
-y = []
+y = [y for y in x if int(y) % 2 == 0]
 
 print(y)
